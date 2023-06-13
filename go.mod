1773aca8ea9d807a0297288a5af0e30683044317
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-06-13 07:12:59 -0700

    1.0.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module mod_app
+
+go 1.20
+
+require github.com/dattaray-basab/go_mod v1.0.0
---
+++ mod_app.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/dattaray-basab/go_mod"
+	"github.com/dattaray-basab/go_mod/pkg1"
+	"github.com/dattaray-basab/go_mod/pkg2"
+)
+
+func main() {
+	go_mod.Hello()
+
+	pkg1.F1()
+	pkg2.F2()
+}
